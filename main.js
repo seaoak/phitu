51c7eb24e6ffeba68ba958db74fe8613d83374e2
Seaoak <seaoak2003@gmail.com>
2018-01-16 16:23:11 +0900

    Add utility functions

--- main.js
+++ main.js
@@ -242,6 +242,47 @@
         };
       },
 
+      last() {
+        const args = Object.freeze(Array.from(arguments));
+        _.assert(args.length === 1);
+        const stacktrace = _.saveStackTrace('_.last():');
+        return function lastHelper() {
+          const args = Object.freeze(Array.from(arguments));
+          _.assert(args.length === 1, args, stacktrace);
+          const seq = args[0];
+          _.assert(_.isSeq(seq), args, stacktrace);
+          if (seq.length === 0) return undefined;
+          return seq[seq.length - 1];
+        };
+      },
+
+      chunk() { // curried version of https://lodash.com/docs/#chunk
+        const args = Object.freeze(Array.from(arguments));
+        _.assert(args.length < 2);
+        const size = (args.length === 0) ? 1 : args[1];
+        _.assert(typeof size === 'number');
+        _.assert(size > 0);
+        return _.recude((acc, x, i, self) => (i % size === 0) ? [].concat(acc, [_.slice(i, i+size)(self)]) : acc, []);
+      },
+
+      fromPairs() { // see http://folktale.origamitower.com/api/v2.1.0/en/folktale.core.object.from-pairs.frompairs.html
+        const args = Object.freeze(Array.from(arguments));
+        _.assertEvery(_.isSeq)(args);
+        _.assertEvery(x => x.length === 2)(args);
+        return _.reduce((acc, x) => Object.defineProperty(acc, x[0], {value: x[1], enumerable: true}), {})(args);
+      },
+
+      seq2obj() {
+        const args = Object.freeze(Array.from(arguments));
+        if (args.length === 0) return {};
+        if (args.length === 1) {
+          _.assert(_.isSeq(args[0]));
+          return _.seq2obj.apply(null, args);
+        }
+        _.assert(args.length % 2 === 0);
+        return _.fromPairs(_.chunk(2)(args));
+      },
+
       toSeq() { // flatten
         const args = Object.freeze(Array.from(arguments));
         _.assert(args.length > 0);
@@ -263,6 +304,24 @@
         _.assert(args.length > 0); // allow to be called by Array.prorotype.filter()
         const arg = args[0];
         return arg && arg.call && arg.call.call && arg.apply && arg.apply.apply;
+      },
+
+      indirectCall() {
+        const args = Object.freeze(Array.from(arguments));
+        _.assert(0 < args.length && args.length < 3);
+        const table = args[0];
+        _.assert(table instanceof Object);
+        const keygen = (args.length === 1) ? _.identity : args[1];
+        _.assert(_.isCallable(keygen));
+        const stacktrace = _.saveStackTrace('_.indirectCall():');
+        return function indirectCallHelper() {
+          const args = Object.freeze(Array.from(arguments));
+          _.assert(args.length > 0, args, stacktrace);
+          const func = table[keygen.apply(null, args)];
+          if (func === undefined) return undefined;
+          _.assert(_.isCallable(func), func, args, table, stacktrace);
+          return func.apply(null, args);
+        };
       },
 
       identity() {
